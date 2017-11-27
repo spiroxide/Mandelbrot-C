eb091a2e798e9e8e2ec94004b11fd5639c93e67f
Erich Ostendarp <sugaroshay@gmail.com>
2017-11-27 05:11:02 -0500

    a

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <cstdio>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
 
 int mandelbrotValue(double real, double imaginary, int maxIterations) {
     double a = real;
@@ -23,22 +27,25 @@ double mapRange(double valF, double startF, double endF, double startT, double e
     return (endT - startT) / (endF - startF) * (valF - startF) + startT;
 }
 
+const int WIDTH = 512, HEIGHT = 512, MAX_ITERATIONS = 100;
+
 int main() {
-    int width = 250;
-    int height = 250;
-    FILE *file = fopen("Mandelbrot.txt", "w");
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            int val = mandelbrotValue(mapRange(x, 0, width, -2, 2), mapRange(y, 0, height, -2, 2), 9);
-            if (val == 0) {
-                fprintf(file, " ");
-            }
-            else {
-                fprintf(file, "%d", val);
+    for (int i = 0; i < 260; i += 10) {
+        string path = "images/mandelbrot" + to_string(i) + ".ppm";
+        ofstream mandelbrotPPM(path);
+        mandelbrotPPM << "P3" << endl;
+        mandelbrotPPM << HEIGHT << " " << WIDTH << endl;
+        mandelbrotPPM << "255" << endl;
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                int val = mandelbrotValue(mapRange(x, 0, WIDTH, -2.5, 1.5), mapRange(y, 0, HEIGHT, -2, 2),
+                                          MAX_ITERATIONS);
+                int r = (val + x + i) % 256;
+                int g = (val + y + i) % 256;
+                int b = (val + (x * y) + i) % 256;
+                mandelbrotPPM << r << " " << g << " " << b << endl;
             }
         }
-        fprintf(file, "\n");
     }
-    fclose(file);
     return 0;
 }
