4e65f08a79cfba9b4b2b2ca8f556cb250798bb72
Erich Ostendarp <sugaroshay@gmail.com>
2018-01-24 02:40:56 -0500

    a

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_aa8e0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_aa8e0.dir/CMakeCCompilerABI.c.obj"
+  "cmTC_aa8e0.pdb"
+  "cmTC_aa8e0.exe"
+  "cmTC_aa8e0.exe.manifest"
+  "libcmTC_aa8e0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_aa8e0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_aa8e0.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_aa8e0.
+# This may be replaced when dependencies are built.
